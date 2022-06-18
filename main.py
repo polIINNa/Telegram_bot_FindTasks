d3cd4a2bdaa402fc966398391c2a266f45247853
Polina <rebrovapolina@gmail.com>
2022-06-18 21:38:22 +0300

    Код с парсингом запихнули в отдельный модуль, добавили кнопки

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import telebot
-from bs4 import BeautifulSoup
-import requests
-
+import parse
+from telebot import types
 
 bot = telebot.TeleBot('5591112751:AAG5kRQfwTFtyJujFHROEa0oXZEkZ2B0XK8')
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    mess = f'Привет, <b>{message.from_user.first_name}</b>. Есть желание подзаработать?'
-    bot.send_message(message.chat.id, mess, parse_mode='html')
-    url = "https://freelance.habr.com/tasks"
-    my_tasks = []
-    text = requests.get(url).text
-    soup = BeautifulSoup(text, 'html.parser')
-    for task in soup.findAll('article', class_="task task_list"):
-        for item in task.findAll('a', class_="tags__item_link"):
-            if item.string.lower() == 'python' or item.string.lower() == "пайтон" or item.string.lower() == "питон":
-                if task not in my_tasks:
-                    my_tasks.append(task)
-                    task_name = task.find('div', 'task__title').string
-                    task_href = task.find('a').get('href')
-                    bot.send_message(message.chat.id, task_name)
-                    bot.send_message(message.chat.id, f'https://freelance.habr.com/{task_href}')
+    mess = f'Хей, <b>{message.from_user.first_name}</b>. Есть желание подзаработать?'
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    but1 = types.KeyboardButton('Да')
+    but2 = types.KeyboardButton('Погнали')
+    markup.add(but1, but2)
+    bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def get_user_text(message):
+    if message.text == 'Привет':
+        bot.send_message(message.chat.id, "И тебе привет, ленивая жопка!", parse_mode='html')
+    elif message.text == 'Да' or message.text == 'Погнали':
+        for i in parse.find_tasks():
+            bot.send_message(message.chat.id, i)
 
 
 bot.polling(none_stop=True)
--- parse.py
+++ parse.py
@@ -2,17 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 
 
-url = "https://freelance.habr.com/tasks"
-my_tasks = []
-text = requests.get(url).text
-soup = BeautifulSoup(text, 'html.parser')
-for task in soup.findAll('article', class_="task task_list"):
-    for item in task.findAll('a', class_="tags__item_link"):
-        if item.string.lower() == 'python' or item.string.lower() == "пайтон" or item.string.lower() == "питон":
-            if task not in my_tasks:
-                my_tasks.append(task)
-                task_name = task.find('div', 'task__title').string
-                task_href = task.find('a').get('href')
-                print(f"Название таска: {task_name}")
-                print(f"Сслыка на таск: https://freelance.habr.com{task_href}")
+def find_tasks():
+    url = "https://freelance.habr.com/tasks"
+    my_tasks = []
+    text = requests.get(url).text
+    soup = BeautifulSoup(text, 'html.parser')
+    for task in soup.findAll('article', class_="task task_list"):
+        for item in task.findAll('a', class_="tags__item_link"):
+            if item.string.lower() == 'python' or item.string.lower() == "пайтон" or item.string.lower() == "питон":
+                if task not in my_tasks:
+                    my_tasks.append(task)
+                    task_name = task.find('div', 'task__title').string
+                    task_href = task.find('a').get('href')
+                    yield (f"Название таска: {task_name}\nСслыка на таск: https://freelance.habr.com{task_href}")
+
 
